922126ee650217b9b792cd7d4ea138d02810474d
Mohammed Hafiz <mohammed.hafiz.475@gmail.com>
2026-01-26 15:23:53 +0530

    chnaged the back button in room bike scene

--- src/components/RoomBikeScene.jsx
+++ src/components/RoomBikeScene.jsx
@@ -230,7 +230,7 @@ export default function RoomBikeScene() {
     return (
         <div className="landing-scene-container">
             <Canvas
-                camera={{ position: [7, 5, 7], fov: 45 }}
+                camera={{ position: [5.5, 4, 5.5], fov: 45 }}
                 shadows
                 gl={{ antialias: true, alpha: true }}
                 dpr={[1, 2]}
@@ -239,7 +239,7 @@ export default function RoomBikeScene() {
                 <OrbitControls
                     enableZoom={true}
                     enablePan={true}
-                    minDistance={5}
+                    minDistance={3}
                     maxDistance={25}
                     minPolarAngle={Math.PI / 6}
                     maxPolarAngle={Math.PI / 2.2}
@@ -362,10 +362,12 @@ export default function RoomBikeScene() {
                     className="landing-back-button"
                     onClick={() => navigate('/windmill')}
                 >
-                    <svg viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2">
-                        <path d="M19 12H5M12 19l-7-7 7-7" />
+                    <svg viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
+                        <path d="M2 6c.6.5 1.2 1 2.5 1C7 7 7 5 9.5 5c2.6 0 2.4 2 5 2 2.5 0 2.5-2 5-2 1.3 0 1.9.5 2.5 1" />
+                        <path d="M2 12c.6.5 1.2 1 2.5 1 2.5 0 2.5-2 5-2 2.6 0 2.4 2 5 2 2.5 0 2.5-2 5-2 1.3 0 1.9.5 2.5 1" />
+                        <path d="M2 18c.6.5 1.2 1 2.5 1 2.5 0 2.5-2 5-2 2.6 0 2.4 2 5 2 2.5 0 2.5-2 5-2 1.3 0 1.9.5 2.5 1" />
                     </svg>
-                    Back
+                    Back to Sea
                 </button>
             </div>
         </div>
